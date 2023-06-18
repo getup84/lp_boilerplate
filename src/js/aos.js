1cd14556848dfd8960309f2246102bebfd182daa
getup84 <get.up.84@gmail.com>
2023-06-18 13:22:57 +0900

    ⚡️ aos追加

---
+++ src/js/aos.js
@@ -0,0 +1,5 @@
+import AOS from 'aos/dist/aos.js';
+import 'aos/dist/aos.css';
+export const aos = () => {
+  AOS.init();
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { hamburger } from './hamburger';
 import { accordion } from './accordion';
 import { modal } from './modal';
+import { aos } from './aos';
 
 hamburger();
 accordion();
 modal();
+aos();
